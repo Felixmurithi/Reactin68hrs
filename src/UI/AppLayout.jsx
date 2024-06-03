aeb9bb2d5289eb8b21007bea8e7d9b8cfe09898e
Felixmurithi <murithifelixgims@gmail.com>
2024-06-03 20:18:23 +0300

    content wrap

--- src/UI/AppLayout.jsx
+++ src/UI/AppLayout.jsx
@@ -3,12 +3,12 @@ import Header from "./Header";
 
 function AppLayout() {
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr]">
+    <page className="grid h-screen min-w-full grid-rows-[auto_1fr]">
       <Header />
-      <main className="align-center min-w-full overflow-y-auto pb-8 pl-[300px] pr-[300px] pt-8 ">
+      <main className="align-center overflow-y min-w-full overflow-x-clip pb-8 pt-8  sm:px-16 md:px-[80px] lg:px-[300px] ">
         <Outlet />
       </main>
-    </div>
+    </page>
   );
 }
 
--- src/UI/Header.jsx
+++ src/UI/Header.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex h-14 justify-end bg-emerald-100 pl-[300px] pr-[300px]">
+    <header className="flex h-14 justify-end bg-emerald-100 sm:px-16 md:px-[80px] lg:px-[300px]">
       <nav className="flex justify-around gap-6   self-center">
         <NavLink to="fundamentals">Fundamentals</NavLink>
         <NavLink to="setup">Set-up</NavLink>
